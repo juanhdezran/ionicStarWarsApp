da9e0c7cdf4ebe24857f02faf091c4912a4bd5eb
juan hernandez <juan.hernandez@mexmax-internet.com>
2019-10-17 13:14:56 -0500

    lesson 5, sql-lite plugin added and implemented in a new service

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
+import { FavoriteService } from 'src/app/services/favorite.service';
 
 @Component({
   selector: 'app-film-details',
@@ -12,13 +13,36 @@ export class FilmDetailsPage implements OnInit {
 
   filmId = null;
   film: any;
+  isFavorite: boolean;
 
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private emailComposer: EmailComposer) { }
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private api: ApiService, 
+    private emailComposer: EmailComposer,
+    private favoriteService: FavoriteService
+  ) { }
 
   ngOnInit() {
     this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getFilm(this.filmId).subscribe( film => {
       this.film = film;
+    });
+    this.favoriteService.isFavorite(this.filmId).then(data => {
+      this.isFavorite =  data;
+    }).catch(err => {
+      //catch errors
+    });
+  }
+
+  favoriteFilm() {
+    this.favoriteService.favoriteFilm(this.filmId).then(() => {
+      this.isFavorite = true;
+    });
+  }
+ 
+  unfavoriteFilm() {
+    this.favoriteService.unfavoriteFilm(this.filmId).then(() => {
+      this.isFavorite = false;
     });
   }
 
---
+++ src/app/services/favorite.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+const STORAGE_KEY = 'favoriteFilms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteService {
+
+  constructor(private storage: Storage) { }
+ 
+  getAllFavoriteFilms() {
+    return this.storage.get(STORAGE_KEY);
+  }
+ 
+  isFavorite(filmId) {
+    return this.getAllFavoriteFilms().then(result => {
+      return result && result.indexOf(filmId) !== -1;
+    });
+  }
+ 
+  favoriteFilm(filmId) {
+    return this.getAllFavoriteFilms().then(result => {
+      if (result) {
+        result.push(filmId);
+        return this.storage.set(STORAGE_KEY, result);
+      } else {
+        return this.storage.set(STORAGE_KEY, [filmId]);
+      }
+    });
+  }
+ 
+  unfavoriteFilm(filmId) {
+    return this.getAllFavoriteFilms().then(result => {
+      if (result) {
+        var index = result.indexOf(filmId);
+        result.splice(index, 1);
+        return this.storage.set(STORAGE_KEY, result);
+      }
+    });
+  }
+}
